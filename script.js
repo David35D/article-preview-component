2ef5cd246510c4009a9aa9525fe6c47335c1d205
David <40966434+Daviddp96@users.noreply.github.com>
2021-11-28 09:43:44 -0400

    Timeout function added
    Added timeout function, attempting to animate the toggle button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let shareButton = document.querySelector('.card-body__bottom');
+let socialsBox = document.querySelector('.card-socials');
 
 let open = false;
 
@@ -7,8 +8,14 @@ function share() {
   if (open) {
     shareButton.classList.remove("off");
     shareButton.classList.add("open");
+    socialsBox.classList.add('card-socials__fade-in');
+    socialsBox.classList.remove('card-socials__fade-out');
   } else {
-    shareButton.classList.add("off");
-    shareButton.classList.remove("open");
+    socialsBox.classList.remove('card-socials__fade-in');
+    socialsBox.classList.add('card-socials__fade-out');
+    setTimeout(function() {
+      shareButton.classList.add("off");
+      shareButton.classList.remove("open");
+    }, 210);
   }
 }
